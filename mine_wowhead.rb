6b549818ebec0caa124835a08318ac308ab5b04f
Tekkub <tekkub@gmail.com>
2008-05-27 20:50:14 -0600

    Mine coords from wowhead as well!

--- mine_wowhead.rb
+++ mine_wowhead.rb
@@ -3,6 +3,7 @@ require 'net/http'
 require 'uri'
 
 zone_ids = []
+coords = {}
 
 Net::HTTP.start("www.wowhead.com") do |http|
 	res = http.get "/?zones=2"
@@ -11,6 +12,16 @@ Net::HTTP.start("www.wowhead.com") do |http|
 	res = http.get "/?zones=3"
 	res.body.scan(/\/\?zone=(\d+)/) {|id| zone_ids << id.first.to_i}
 	puts "Found #{zone_ids.size} instances and raids"
+
+	zone_ids.each do |id|
+		coords[id] = []
+		res = http.get "/?zone=#{id}"
+		res.body.scan(/\[([\d.]+),([\d.]+),\{label:'([^']*)',type:'([^']*)'\}\]/) {|m| coords[id] << m.join("_")}
+		res.body.scan(/\[([\d.]+),([\d.]+),\{label:'<b class="q">([^<]*)<\/b>/) {|m| coords[id] << m.join("_")}
+	end
+
+	coords = [*coords].reject {|c| c[1].empty?}.map {|c| "#{c[0]}:#{c[1].join("|")}"}.join("\n")
+	open("Coords.lua", "wb") {|f| f << "local coords = [[\n" << coords << "\n]]\n"}
 end
 
 Net::HTTP.start("static.wowhead.com") do |http|
